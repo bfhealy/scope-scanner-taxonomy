fbc350ee3483bf40a3057c4738ed44e22679ff6a
Brian Healy <brian.francis.healy@gmail.com>
2022-11-16 18:20:35 -0600

    Initial scanner taxonomy

---
+++ scope_scanner/__init__.py
@@ -0,0 +1,19 @@
+"""
+This module contains the Scope scanner taxonomy.
+"""
+
+__version__ = '1.0.0'
+__all__ = ["__version__", "taxonomy", "name", "provenance"]
+
+import os
+from os.path import join
+import yaml
+from yaml import FullLoader
+
+tax_path = join(os.path.dirname(__file__), 'scanner.yaml')
+
+with open(tax_path) as taxonomy_yaml:
+    taxonomy = yaml.load(taxonomy_yaml, Loader=yaml.FullLoader)
+
+name = 'SCoPe Scanner Taxonomy'
+provenance = 'https://github.com/bfhealy/scope-scanner-taxonomy'
